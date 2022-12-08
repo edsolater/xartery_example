06ce59650fd31c7e1bc4d6efdd24d6bba500954c
edsolater <edsolater@qq.com>
2022-12-08 21:47:27 +0800

    🚧 motion can start from middle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -66,41 +66,47 @@ export interface MotionProps {
   children?: DivChildNode
 }
 
+function calcDeltaTrasformCSS(from: DOMRect, to: DOMRect) {
+  // get the difference in position
+  const deltaX = to.x - from.x
+  return `translate(${-deltaX}px)`
+}
 export const Motion = uikit('Motion', ({ children }: MotionProps) => {
   const squareRef = useRef<HTMLElement>()
-  const initialPositionRef = useRef<{ x: number; y: number }>()
-
-  const cachedAnimation = useRef<Animation>()
-
+  const fromRect = useRef<DOMRect>()
   useLayoutEffect(() => {
     // so css change must cause rerender by React, so useLayoutEffect can do something before change attach to DOM
     if (!squareRef.current) return
 
-    const box = squareRef.current.getBoundingClientRect()
-    if (initialPositionRef.current && box && moved(initialPositionRef.current, box)) {
-      // stop prev animation
-      cachedAnimation.current?.cancel()
+    const toRect = squareRef.current.getBoundingClientRect()
 
-      // get the difference in position
-      const deltaX = box.x - initialPositionRef.current.x
-
-      // animate back to the final position
-      const animationControl = squareRef.current.animate(
-        [{ transform: `translate(${-deltaX}px)` }, { transform: '', offset: 1 }],
-        { duration: 300, iterations: 1 } // iteration 1 can use to moke transition
+    let animationControl: Animation | undefined
+    if (fromRect.current && toRect && hasRectChanged(fromRect.current, toRect)) {
+      animationControl = squareRef.current.animate(
+        [{ transform: calcDeltaTrasformCSS(fromRect.current, toRect) }, { transform: '', offset: 1 }],
+        { duration: 300, iterations: 1 , easing:'ease'} // iteration 1 can use to moke transition
       )
-
-      // React store the controller
-      cachedAnimation.current = animationControl
     }
 
-    initialPositionRef.current = box
+    return () => {
+      if (animationControl?.playState === 'finished') {
+        // record for next frame
+        fromRect.current = toRect
+      } else {
+        // record for next frame
+        fromRect.current = squareRef.current?.getBoundingClientRect()
+        animationControl?.cancel()
+      }
+    }
   })
 
   return <AddProps domRef={squareRef}>{children}</AddProps>
 })
 
-const moved = (initialBox: { x: number; y: number } | undefined, finalBox: { x: number; y: number } | undefined) => {
+const hasRectChanged = (
+  initialBox: { x: number; y: number } | undefined,
+  finalBox: { x: number; y: number } | undefined
+) => {
   // we just mounted, so we don't have complete data yet
   if (!initialBox || !finalBox) return false
   const xMoved = initialBox.x !== finalBox.x
